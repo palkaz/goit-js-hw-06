0c8bb84ccfeec636494aa4af399d172fc07b0b75
palkaz <97454178+palkaz@users.noreply.github.com>
2022-07-20 21:11:51 +0300

    task2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+const listIngEl = document.createElement("li");
+ingredients.forEach(function (el) {
+  const listIngEl = document.createElement("li");
+  listIngEl.textContent = el;
+  list.append(listIngEl);
+  listIngEl.classList.add("item");
+  // console.log(el);
+  // console.log(index);
+});
