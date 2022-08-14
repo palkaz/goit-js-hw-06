dae1288ba5dd333bded87d3611b3185fa8d68dc7
palkaz <97454178+palkaz@users.noreply.github.com>
2022-08-14 17:40:47 +0300

    task8 completed

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,28 @@
+// находим елемент с которым работаем
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  // отменяем действие по умолчанию
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  //   if (email.value === "" || password.value === "") {
+  //     return console.log("Заполните все поля!");
+  //   }
+  // проверка заполнения формы
+  if (email.value === "") {
+    return console.log("Заполните поле Email!");
+  }
+  if (password.value === "") {
+    return console.log("Заполните поле Password!");
+  }
+  const dataFofm = { email: `${email.value}`, password: `${password.value}` };
+  console.log(dataFofm);
+
+  // отчистка формы
+  event.currentTarget.reset();
+}
