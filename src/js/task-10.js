a5ffd35c7e0a4dcc666de29c4e6d6ba85a3d99e2
palkaz <97454178+palkaz@users.noreply.github.com>
2022-08-14 19:59:50 +0300

    task10- начало

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,24 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  inputEl: document.querySelector("input"),
+  btnCreate: document.querySelector("button[data-create]"),
+  btnDestroy: document.querySelector("button[data-destroy]"),
+  container: document.querySelector("#boxes"),
+};
+
+refs.btnCreate.addEventListener("click", onBtnCreateClick);
+// refs.btnDestroy.addEventListener("click", onBtnDestroyClick);
+// refs.inputEl.querySelector("click", onInputChange);
+
+function onBtnCreateClick(event) {
+  const numberDiv = event.currentTarget.value;
+
+  function createBoxes(amount) {
+    const divCreated = document.createElement("div");
+    amount = numberDiv;
+    refs.container.append(divCreated);
+  }
+}
