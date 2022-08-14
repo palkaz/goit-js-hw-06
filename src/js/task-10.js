1c6304c9375068cac96c45f8f0a3e603eb28769d
palkaz <97454178+palkaz@users.noreply.github.com>
2022-08-15 01:59:35 +0300

    task10 completed

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -2,23 +2,37 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const refs = {
-  inputEl: document.querySelector("input"),
-  btnCreate: document.querySelector("button[data-create]"),
-  btnDestroy: document.querySelector("button[data-destroy]"),
-  container: document.querySelector("#boxes"),
+// функция для случайного цвета через RGB
+// function getRandomRgbColor() {
+//   const red = Math.round(Math.random(0, 1) * 255);
+//   const green = Math.round(Math.random(0, 1) * 255);
+//   const blue = Math.round(Math.random(0, 1) * 255);
+//   return `rgb(${red}, ${green}, ${blue})`;
+// }
+
+const ref = {
+  inputValue: document.querySelector("#controls>input"),
+  btnGreate: document.querySelector("[data-create]"),
+  btnDestroy: document.querySelector("[data-destroy]"),
+  boxListHTML: document.querySelector("#boxes"),
 };
 
-refs.btnCreate.addEventListener("click", onBtnCreateClick);
-// refs.btnDestroy.addEventListener("click", onBtnDestroyClick);
-// refs.inputEl.querySelector("click", onInputChange);
-
-function onBtnCreateClick(event) {
-  const numberDiv = event.currentTarget.value;
-
-  function createBoxes(amount) {
-    const divCreated = document.createElement("div");
-    amount = numberDiv;
-    refs.container.append(divCreated);
+const createBoxes = (amount) => {
+  const boxWrap = [];
+  amount = ref.inputValue.value;
+  for (let i = 0; i < amount; i += 1) {
+    const boxSize = 30;
+    let growBox = boxSize + i * 10;
+    const boxItem = `<div style ='width:${growBox}px ;height:${growBox}px; background-color:${getRandomHexColor()}'></div>`;
+    boxWrap.push(boxItem);
   }
-}
+  ref.boxListHTML.insertAdjacentHTML("afterbegin", boxWrap.join(""));
+};
+// console.log(ref.boxWrap);
+const destroyBoxes = () => {
+  ref.boxListHTML.innerHTML = "";
+  ref.inputValue.value = "";
+};
+
+ref.btnGreate.addEventListener("click", createBoxes);
+ref.btnDestroy.addEventListener("click", destroyBoxes);
