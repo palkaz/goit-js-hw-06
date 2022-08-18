42193b77efc7bb3c3b0e087636c61827c94b8a6c
palkaz <97454178+palkaz@users.noreply.github.com>
2022-08-18 10:46:41 +0300

    task1  corrected

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,22 @@
-const NumOfCatEl = document.querySelectorAll("h2");
-console.log("Number of categories:", NumOfCatEl.length);
-let numCatEl = NumOfCatEl.length;
-console.log("");
-// --------------------------------------
-NumOfCatEl.forEach(function (number, index) {
-  // console.log("Category:", number.textContent);
+// const NumOfCatEl = document.querySelectorAll("h2");
+// console.log("Number of categories:", NumOfCatEl.length);
+// let numCatEl = NumOfCatEl.length;
+// console.log("");
+// // --------------------------------------
+// NumOfCatEl.forEach(function (number, index) {
+//   // console.log("Category:", number.textContent);
 
-  const navItemNumEl = document.querySelectorAll("li.item");
-  console.log("Category:", navItemNumEl[index].children[0].textContent);
-  console.log("Elements:", navItemNumEl[index].children[1].children.length);
-  console.log("");
+//   const navItemNumEl = document.querySelectorAll("li.item");
+//   console.log("Category:", navItemNumEl[index].children[0].textContent);
+//   console.log("Elements:", navItemNumEl[index].children[1].children.length);
+//   console.log("");
+// });
+// вар2
+const ulEl = document.querySelectorAll(".item");
+const numberOfulEl = ulEl.length;
+console.log(`Number of categories: ${numberOfulEl}`);
+
+const mainUl = ulEl.forEach(el =>{
+   console.log(`Category: ${el.firstElementChild.textContent}`);
+   console.log(`Element: ${el.lastElementChild.children.length}`);
 });
