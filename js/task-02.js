ec9aee67774453d1dd86c9a34fd7d346521fedeb
palkaz <97454178+palkaz@users.noreply.github.com>
2022-08-20 04:46:02 +0300

    task2 process

--- js/task-02.js
+++ js/task-02.js
@@ -7,6 +7,7 @@ const ingredients = [
   "Condiments",
 ];
 const list = document.querySelector("#ingredients");
+// вар1
 // const listIngEl = document.createElement("li");
 // ingredients.forEach(function (el) {
 //   const listIngEl = document.createElement("li");
@@ -16,9 +17,19 @@ const list = document.querySelector("#ingredients");
 //   console.log(el);
 //   // console.log(index);
 // });
+// вар2 через шаблон
+// const listIngEl =ingredients.map(
+//     ingredient =>
+//       `<li class="item">${ingredient}</li>`)
+//       .join("");
+// list.insertAdjacentHTML("beforeend", listIngEl);
+// вар3 через распыление
 
-const listIngEl =ingredients.map(
-    ingredient =>
-      `<li class="item">${ingredient}</li>`)
-      .join("");
-list.insertAdjacentHTML("beforeend", listIngEl);
+ const listIngEl =ingredients.map(
+      ingredient =>
+      document.createElement("li")
+  .listIngEl.textContent(ingredient)
+      .listIngEl.classList.add("item")
+      )
+        .join("");
+       list.append(...listIngEl); 
